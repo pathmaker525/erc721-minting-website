0f569b9d72ce03dd988ba14b620a615dfcdbf11e
PathMaker <pathmaker.525@gmail.com>
2022-04-11 20:03:29 +0900

    changed address of sol on rinkeby

--- src/configs/Constants/Contracts.js
+++ src/configs/Constants/Contracts.js
@@ -9,7 +9,7 @@ export const contractAddresses = {
       "https://etherscan.io/token/0x7b9C9C846a5a9ADaAab0B6cc224f9AF47405F721",
   },
   [ETH_TESTNET]: {
-    address: "0x30C4Df9E5323B1844bA91573B4966E7Af9A93683",
+    address: "0x575702DAd53D4681C45EDD33D349c6e10C2C9d49",
     explorerUrl:
       "https://rinkeby.etherscan.io/token/0x30C4Df9E5323B1844bA91573B4966E7Af9A93683",
   },
