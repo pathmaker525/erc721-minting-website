d91336dfcd562f1ccbab08d88bb953b24ed7286e
PathMaker <pathmaker.525@gmail.com>
2022-04-12 07:55:12 +0900

    added hash

--- contracts/TheShmurfs.sol
+++ contracts/TheShmurfs.sol
@@ -6,8 +6,8 @@
 //     ██║   ███████║█████╗      ███████╗███████║██╔████╔██║██║   ██║██████╔╝█████╗  ███████╗
 //     ██║   ██╔══██║██╔══╝      ╚════██║██╔══██║██║╚██╔╝██║██║   ██║██╔══██╗██╔══╝  ╚════██║
 //     ██║   ██║  ██║███████╗    ███████║██║  ██║██║ ╚═╝ ██║╚██████╔╝██║  ██║██║     ███████║
-//     ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝                                                                                          
-                                                                             
+//     ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝
+
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -36,13 +36,14 @@ contract TheShmurfs is Ownable, ERC721A {
   address private fundWallet    = payable(0xc7f9aDaec08aBB1ac05d8f8Fd6015A0912b43392); // Owner Fund Wallet Address
   address private devOneWallet  = payable(0xbC937543371F759867acf29A3391dEecF12f3d76); // Developer One Wallet Address
 
-  uint private whitelistMintPassKey = 0;
+  string private whitelistMintPassKey;
 
   mapping(address => uint8) private whitelistBuyList;
 
   constructor() ERC721A("The Shmurfs", "SHMURFS") {
     sellingStep = Step.Before;
     revealed = false;
+    whitelistMintPassKey = getHash("0");
   }
 
   modifier callerIsUser() {
@@ -60,13 +61,13 @@ contract TheShmurfs is Ownable, ERC721A {
     _;
   }
 
-  function whitelistMint(uint8 quantity, uint mintPasskey_) external payable callerIsUser whitelistSaleIsOpen {
-    require(whitelistMintPassKey == mintPasskey_, "WHITELIST_SALE: Passkey not correct");
+  function whitelistMint(uint8 quantity, string calldata mintPasskey_) external payable callerIsUser whitelistSaleIsOpen {
+    require(keccak256(abi.encodePacked(whitelistMintPassKey)) == keccak256(abi.encodePacked(getHash(mintPasskey_))), "WHITELIST_SALE: Passkey not correct");
     require(totalSupply() < MAX_SUPPLY, "WHITELIST_SALE: Please go to the Opensea to buy The Shmurfs.");
     require(totalSupply() + quantity <= MAX_SUPPLY, "WHITELIST_SALE: Max supply exceeded");
     require(whitelistBuyList[msg.sender] + quantity <= MAX_WHITELIST_BATCH, "WHITELIST_SALE: You can not get more than 3 NFTs on the Whitelist Sale");
     require(msg.value >= PRICE_WHITELIST * quantity, "WHITELIST_SALE: Not enough funds");
-    
+
     whitelistBuyList[msg.sender] += quantity;
     _safeMint(msg.sender, quantity);
 
@@ -82,29 +83,41 @@ contract TheShmurfs is Ownable, ERC721A {
     refundIfOver(PRICE_PUBLIC * quantity);
   }
 
-  function setWhitelistMintPassKey(uint mintPasskey_) external onlyOwner {
+  function setWhitelistMintPassKey(string calldata mintPasskey_) external onlyOwner {
     whitelistMintPassKey = mintPasskey_;
   }
 
-  function setSellingStep(Step _step) external onlyOwner {
-    sellingStep = _step;
+  function setStepAsBefore() external onlyOwner {
+    sellingStep = Step.Before;
+  }
+
+  function setStepAsWhitelistSale() external onlyOwner {
+    sellingStep = Step.WhitelistSale;
+  }
+
+  function setStepAsPublicSale() external onlyOwner {
+    sellingStep = Step.PublicSale;
+  }
+
+  function setStepAsAfter() external onlyOwner {
+    sellingStep = Step.After;
   }
 
   // metadata URI
-  function setBaseURI(string calldata _baseuri) external onlyOwner {
-    baseURI = _baseuri;
-  }
-
-  function _baseURI() internal view override returns (string memory) {
-    return baseURI;
-  }
-
   function setUnrevealedImageURL(string calldata _imageURL) external onlyOwner {
     unrevealedImageURL = _imageURL;
   }
 
   function setRevealed(bool _reveal) external onlyOwner {
     revealed = _reveal;
+  }
+
+  function setBaseURI(string calldata _baseuri) external onlyOwner {
+    baseURI = _baseuri;
+  }
+
+  function _baseURI() internal view override returns (string memory) {
+    return baseURI;
   }
 
   function tokenURI(uint _tokenId) public view override returns (string memory) {
@@ -149,5 +162,22 @@ contract TheShmurfs is Ownable, ERC721A {
       return PRICE_PUBLIC;
     else
       return PRICE_WHITELIST;
+  }
+
+  // Security for whitelist
+  function getHash(string memory _string) public pure returns(string memory) {
+     return bytes32ToString(keccak256(abi.encodePacked(_string)));
+  }
+
+  function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+    uint8 i = 0;
+    while(i < 32 && _bytes32[i] != 0) {
+        i++;
+    }
+    bytes memory bytesArray = new bytes(i);
+    for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
+        bytesArray[i] = _bytes32[i];
+    }
+    return string(bytesArray);
   }
 }
